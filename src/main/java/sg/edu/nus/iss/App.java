61685965479cbdedb99ae33d9f7029202695c1fb
Bryan Chua <Bryanchua.1994@gmail.com>
2023-04-18 16:51:21 +0800

    day02_workshop

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // System.out.println( "Hello World!" );
+
+       BankAccount bryBankAccount = new BankAccount("brybry","001-000-1111",10000);
+       System.out.println(bryBankAccount.toString());
+
+       bryBankAccount.deposit(10000);
+       System.out.println(bryBankAccount.toString());
+
+       bryBankAccount.withdraw(99);
+       System.out.println(bryBankAccount.toString());
+
     }
 }
